6ec118315e975c0070dacfd3d2160aba59493228
Paul Hummer <paul@eventuallyanyway.com>
2016-05-11 10:56:52 -0600

    More database support for heroku

--- udoco/settings.py
+++ udoco/settings.py
@@ -100,11 +100,9 @@ WSGI_APPLICATION = 'udoco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=500)
 }
 
 
