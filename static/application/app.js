18a999a79db3e8f354a48faa82b41cdc90f90551
Paul Hummer <paul@eventuallyanyway.com>
2017-04-24 19:43:26 -0600

    Fix indentation for the cookie application.

--- static/application/app.js
+++ static/application/app.js
@@ -3,17 +3,17 @@ window.App = window.App || {};
 App.init = () => {
   function getCookie(name) {
     var cookieValue = null;
-      if (document.cookie && document.cookie !== '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = jQuery.trim(cookies[i]);
-              // Does this cookie string begin with the name we want?
-              if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              }
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+            }
           }
-      }
+        }
       return cookieValue;
   }
 
