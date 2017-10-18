7e017b592705653d777dba2b9d4c1fa25a108c50
Paul Hummer <paul@eventuallyanyway.com>
2017-10-18 01:29:18 -0600

    Disable service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>), document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
