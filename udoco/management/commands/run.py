0b09d454d993b8c085e911387bb07c72b5e09f82
Paul Hummer <paul@eventuallyanyway.com>
2017-10-18 01:28:00 -0600

    Hook up js build to the run management command

--- udoco/management/commands/run.py
+++ udoco/management/commands/run.py
@@ -10,15 +10,15 @@ from django.contrib.staticfiles.management.commands.runserver import Command\
 class Command(StaticfilesRunserverCommand):
 
     def inner_run(self, *args, **options):
-        self.start_grunt()
+        self.start_frontend()
         return super(Command, self).inner_run(*args, **options)
 
-    def start_grunt(self):
-        self.grunt_process = subprocess.Popen(
-            ['npm run watch'],
+    def start_frontend(self):
+        self._process = subprocess.Popen(
+            ['cd frontend && npm run watch integrate'],
             shell=True,
             stdin=subprocess.PIPE,
             stdout=self.stdout,
             stderr=self.stderr,
         )
-        atexit.register(os.kill, self.grunt_process.pid, signal.SIGTERM)
+        atexit.register(os.kill, self._process.pid, signal.SIGTERM)
