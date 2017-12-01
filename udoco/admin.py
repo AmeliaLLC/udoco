f6d55cb74396ede9e0124a5ddd4766b4170e460c
Paul Hummer <paul@eventuallyanyway.com>
2017-12-01 12:32:56 -0700

    Housekeeping

--- udoco/admin.py
+++ udoco/admin.py
@@ -42,14 +42,14 @@ class LeagueAdmin(admin.ModelAdmin):
                 'name', 'abbreviation',)}),
         ('Scheduling', {
             'description': 'The users with scheduling rights for this league.',
-            'fields': (
-                'schedulers',)}))
+            'fields': ('schedulers',),
+        }))
 
 
 # Below are some dummy admin models to hide some boilerplate models.
 from django.contrib.auth.models import Group
 admin.site.unregister(Group)
-#from social_django.models import Association, Nonce, UserSocialAuth  # NOQA
-#admin.site.unregister(Association)
-#admin.site.unregister(Nonce)
-#admin.site.unregister(UserSocialAuth)
+from social_django.models import Association, Nonce, UserSocialAuth  # NOQA
+admin.site.unregister(Association)
+admin.site.unregister(Nonce)
+admin.site.unregister(UserSocialAuth)
