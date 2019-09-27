89da64af6fc8ea180c52291f0fe3331f6764ef2c
Paul Hummer <paul@eventuallyanyway.com>
2019-09-27 12:20:54 -0600

    Fix: Don't use the worker code
    The service worker code was causing issues in newer browsers. This patch
    removes the import of `registerServiceWorker.tsx` as well as the call to
    `registerServiceWorker`.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,9 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>, document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
